eaf8688616bc0b48e224bf009f0be3914494d67e
guillermosifu <sifuentes1201@gmail.com>
2023-08-08 20:32:26 -0500

    darmode

--- semana7/dia2/DarkMode/main.js
+++ semana7/dia2/DarkMode/main.js
@@ -1,2 +1,15 @@
+// stamos capturando el elemento a usar
 
 const btn = document.querySelector(".btn");
+const mensaje = document.querySelector("#mensaje");
+
+//estamos declarando un evento y su funcion
+btn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+
+  if (document.documentElement.classList.contains("dark-theme")) {
+    mensaje.textContent = "estas en modo Oscuro";
+  } else {
+    mensaje.textContent = "estas en modo Light";
+  }
+});
