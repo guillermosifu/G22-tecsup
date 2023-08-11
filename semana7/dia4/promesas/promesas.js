7e86c3e3f7c48c2e5a307a6b64b25e592e8e6ef0
guillermosifu <sifuentes1201@gmail.com>
2023-08-10 20:37:40 -0500

    primise

---
+++ semana7/dia4/promesas/promesas.js
@@ -0,0 +1,28 @@
+// promesa  sirve para podr ejecutar algo asyncrono
+// cunado hacemos una petocio e una API esta tiene o pued etenr n tiempo de espera
+
+//// la primera forma de usar una promesa
+// la clase que se ecarga de instanciar se llama new Promise()
+
+//resolve : es el envargado de retornar la respuesta cando todo este correcto
+// reject :es el enargado de retornar le repuesta cuando todo esete mal
+// ambas funciones retornarn algo
+// cuando creamos una promesa esta no tiene return
+
+const promesa = new Promise((resolve, reject) => {
+  //vamos a simular un tiempo de espera
+  setTimeout(() => {
+    //esta funcion va esperar 3 segundos par poder retorna el resolve
+    resolve("todo bien cumpli tu promesa");
+  }, 5000);
+});
+
+//como hacemos para ejecutar la promesa
+
+async function getDataFromPromise(){
+    const respuesta = await promesa;
+    console.log(respuesta)
+}
+
+
+getDataFromPromise()
